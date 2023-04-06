653e1cf40d02df0d09510d40b4eecab5cd8e76c3
AbhijitL <labhijitsingh13@gmail.com>
2023-04-06 23:58:31 +0530

    getRandomImage path

--- lib/services/utils.dart
+++ lib/services/utils.dart
@@ -12,3 +12,16 @@ Future<Color> _updatePaletteGenerator(Image img) async {
   );
   return paletteGenerator.dominantColor!.color;
 }
+
+Future<String> getRandomImageLoc() async {
+  var images = [
+    "assets/images/test_images/test_cover.JPG",
+    "assets/images/test_images/test_cover1.JPG",
+    "assets/images/test_images/test_cover2.JPG",
+    "assets/images/test_images/test_cover3.JPG",
+    "assets/images/test_images/test_cover4.JPG",
+    "assets/images/test_images/test_cover5.JPG"
+  ];
+  var imageFilePath = await (images..shuffle()).first;
+  return imageFilePath;
+}
