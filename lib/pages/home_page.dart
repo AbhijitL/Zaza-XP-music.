6d72c0670a94d11f1bce18a7f4f515520fb3f501
AbhijitL <labhijitsingh13@gmail.com>
2023-04-07 01:03:56 +0530

    added song title and artist title

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:zaza_xp/services/utils.dart';
 import 'package:zaza_xp/widgets/song_cover.dart';
 
@@ -37,12 +38,44 @@ class _HomePageState extends State<HomePage> {
         colors: <Color>[
           imageColor,
           Color(0xff000000),
-          Color(0xff000000),
+          Color.fromARGB(255, 0, 0, 0),
         ],
       )),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
-        children: [SongCover(imgUrl: imgPath)],
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SongCover(imgUrl: imgPath),
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Material(
+              color: Colors.transparent,
+              child: Text(
+                "Song Title",
+                maxLines: 1,
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontFamily: GoogleFonts.jetBrainsMono().fontFamily,
+                  fontSize: 50,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          Material(
+            color: Colors.transparent,
+            child: Text(
+              "Artist Name",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 18,
+                fontFamily: GoogleFonts.jetBrainsMono().fontFamily,
+                color: Color.fromARGB(255, 232, 232, 232),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
