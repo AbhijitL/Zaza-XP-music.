c9a9b8ec2c371200616d59888661ab878d8da813
AbhijitL <labhijitsingh13@gmail.com>
2023-04-06 23:59:02 +0530

    song_cover string imgUrl constructor

--- lib/widgets/song_cover.dart
+++ lib/widgets/song_cover.dart
@@ -1,13 +1,19 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class SongCover extends StatefulWidget {
-  const SongCover({super.key});
-
+  String imgUrl;
+  SongCover({
+    Key? key,
+    required this.imgUrl,
+  }) : super(key: key);
   @override
   State<SongCover> createState() => _SongCoverState();
 }
 
 class _SongCoverState extends State<SongCover> {
+  var imageFilePath = "assets/images/test_images/test_cover.JPG";
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,7 +25,7 @@ class _SongCoverState extends State<SongCover> {
         child: FittedBox(
           alignment: Alignment.center,
           fit: BoxFit.cover,
-          child: Image.asset("assets/images/test_cover.JPG"),
+          child: Image.asset(widget.imgUrl),
           clipBehavior: Clip.hardEdge,
         ),
       ),
