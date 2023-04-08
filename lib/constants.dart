969be3b06ce37382e614675b259e920cfac41a75
AbhijitL <labhijitsingh13@gmail.com>
2023-04-08 16:09:29 +0530

    added constants

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+var images = [
+  "assets/images/test_images/test_cover.JPG",
+  "assets/images/test_images/test_cover1.JPG",
+  "assets/images/test_images/test_cover2.JPG",
+  "assets/images/test_images/test_cover3.JPG",
+  "assets/images/test_images/test_cover4.JPG",
+  "assets/images/test_images/test_cover5.JPG"
+];
+
+String defaultImage = images[1];
