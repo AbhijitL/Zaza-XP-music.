d4acffa1246c9a130d3606800739674d6644acc5
AbhijitL <labhijitsingh13@gmail.com>
2023-04-08 20:29:42 +0530

    formate seconds to M:S

--- lib/services/utils.dart
+++ lib/services/utils.dart
@@ -19,3 +19,11 @@ Future<String> getRandomImageLoc() async {
   var imageFilePath = await (images..shuffle()).first;
   return imageFilePath;
 }
+
+String formatedTime({required int timeInSecond}) {
+  int sec = timeInSecond % 60;
+  int min = (timeInSecond / 60).floor();
+  String minute = min.toString().length <= 1 ? "0$min" : "$min";
+  String second = sec.toString().length <= 1 ? "0$sec" : "$sec";
+  return "$minute:$second";
+}
