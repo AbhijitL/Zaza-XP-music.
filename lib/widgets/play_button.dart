f79548f3982675fb30b14fbf3576dd2e26d8631f
AbhijitL <labhijitsingh13@gmail.com>
2023-04-08 22:09:54 +0530

    play audio from the stream

--- lib/widgets/play_button.dart
+++ lib/widgets/play_button.dart
@@ -1,5 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:zaza_xp/constants.dart';
+
+final audioPlayer = AudioPlayer();
 
 class PlayButton extends StatefulWidget {
   Color btnColor;
@@ -12,12 +16,18 @@ class PlayButton extends StatefulWidget {
 }
 
 class _PlayButtonState extends State<PlayButton> {
-  bool isPlaying = false;
+  bool isPlaying = true;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    // audioPlayer.onPlayerStateChanged.listen((event) {
+    //   setState(() async {
+    //     isPlaying = event == PlayerState.PLAYING;
+    //   });
+    // });
+    audioPlayer.play(MusicSource, position: null);
   }
 
   @override
@@ -26,7 +36,17 @@ class _PlayButtonState extends State<PlayButton> {
       child: Material(
         color: Colors.transparent,
         child: IconButton(
-          onPressed: () {},
+          onPressed: () async {
+            if (isPlaying) {
+              isPlaying = false;
+              await audioPlayer.setVolume(0);
+            } else {
+              isPlaying = true;
+              await audioPlayer.setVolume(1);
+              await audioPlayer.play(MusicSource, position: null);
+            }
+            setState(() {});
+          },
           icon: Icon(
             isPlaying
                 ? Icons.pause_circle_filled_rounded
