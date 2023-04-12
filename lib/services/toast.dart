3f1b3438e2fe937f3e4728de0817efebb0f3ac72
AbhijitL <labhijitsingh13@gmail.com>
2023-04-12 21:44:36 +0530

    call native toast

---
+++ lib/services/toast.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/services.dart';
+
+class Toast {
+  Toast(String message) {
+    _showToast(message);
+  }
+  static const platform = const MethodChannel("flutter.toast.message.channel");
+  Future<Null> _showToast(String message) async {
+    // invoke method
+    await platform.invokeMethod('toast', {'message': message});
+  }
+}
