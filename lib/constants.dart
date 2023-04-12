6906087cbf5094a2d9079f96e5354c9f40fc9f3b
AbhijitL <labhijitsingh13@gmail.com>
2023-04-12 18:25:40 +0530

    update default image

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 const MusicSource = 'https://listen.moe/fallback';
 const VocaloidSource = "http://curiosity.shoutca.st:8019/stream";
 
-const String defaultImage = "https://i.imgur.com/GdJp4v0.png";
+const String defaultImage = "https://i.imgur.com/RwKMSn9.png";
