a56a47462919de0e07d39eac0e0b9dec57243760
AbhijitL <labhijitsingh13@gmail.com>
2023-04-12 22:04:03 +0530

    TapablleText when tap do copy to clipboard op.

---
+++ lib/widgets/tapable_text.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:zaza_xp/services/toast.dart';
+
+class TapableText extends StatelessWidget {
+  String text;
+  double fontSize;
+  Color textColor;
+  String copyText;
+  String message;
+  TapableText({
+    Key? key,
+    required this.text,
+    required this.fontSize,
+    required this.textColor,
+    required this.copyText,
+    required this.message,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Material(
+        color: Colors.transparent,
+        child: Text(
+          text,
+          maxLines: 1,
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            fontSize: fontSize,
+            color: textColor,
+          ),
+        ),
+      ),
+      onTap: () async {
+        await Clipboard.setData(ClipboardData(text: copyText));
+        Toast(message);
+      },
+    );
+  }
+}
