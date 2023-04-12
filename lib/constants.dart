507ac1528d94087e47757633a20c70a738bcf5b9
AbhijitL <labhijitsingh13@gmail.com>
2023-04-12 18:48:14 +0530

    appversion

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,5 @@
-const MusicSource = 'https://listen.moe/fallback';
-const VocaloidSource = "http://curiosity.shoutca.st:8019/stream";
+const String AppVersion = "0.2A";
+const String MusicSource = 'https://listen.moe/fallback';
+const String VocaloidSource = "http://curiosity.shoutca.st:8019/stream";
 
 const String defaultImage = "https://i.imgur.com/RwKMSn9.png";
