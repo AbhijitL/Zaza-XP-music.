0436e874a2afe16e74ac21190e52a213163c7399
AbhijitL <labhijitsingh13@gmail.com>
2023-04-14 23:22:27 +0530

    minor changes adding const

--- lib/widgets/play_button.dart
+++ lib/widgets/play_button.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:zaza_xp/constants.dart';
+
 import 'package:audio_service/audio_service.dart';
 import '../services/service_locator.dart';
 
@@ -30,31 +30,28 @@ class _PlayButtonState extends State<PlayButton> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Material(
-        color: Colors.transparent,
-        child: IconButton(
-          onPressed: () async {
-            if (isPlaying) {
-              isPlaying = false;
-              _audioHandler.pause();
-            } else {
-              isPlaying = true;
-              _audioHandler.play();
-            }
-            setState(() {});
-          },
-          icon: Icon(
-            isPlaying
-                ? Icons.pause_circle_filled_rounded
-                : Icons.play_circle_fill_rounded,
-            color: widget.btnColor,
-            size: 100,
-            shadows: <Shadow>[
-              Shadow(
-                  color: Color.fromARGB(153, 141, 141, 141), blurRadius: 50.0)
-            ],
-          ),
+    return Material(
+      color: Colors.transparent,
+      child: IconButton(
+        onPressed: () async {
+          if (isPlaying) {
+            isPlaying = false;
+            _audioHandler.pause();
+          } else {
+            isPlaying = true;
+            _audioHandler.play();
+          }
+          setState(() {});
+        },
+        icon: Icon(
+          isPlaying
+              ? Icons.pause_circle_filled_rounded
+              : Icons.play_circle_fill_rounded,
+          color: widget.btnColor,
+          size: 100,
+          shadows: const <Shadow>[
+            Shadow(color: Color.fromARGB(153, 141, 141, 141), blurRadius: 50.0)
+          ],
         ),
       ),
     );
